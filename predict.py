3d98fc1dd880a635a61c93af0e237eb649ea607e
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-12-01 11:56:26 -0800

    adding hubert

--- predict.py
+++ predict.py
@@ -10,30 +10,7 @@ class ModelOutput(BaseModel):
 class Predictor(BasePredictor):
 
     def setup(self):
-        """Load the model into memory to make running multiple predictions efficient"""
-
-        # SETUP
-        large_quant_model = False  # Use the larger pretrained model
-        device = 'cuda'  # 'cuda', 'cpu', 'cuda:0', 0, -1, torch.device('cuda')
-
-        import numpy as np
-        import torch
-        import torchaudio
-        from encodec import EncodecModel
-        from encodec.utils import convert_audio
-        from bark_hubert_quantizer.hubert_manager import HuBERTManager
-        from bark_hubert_quantizer.pre_kmeans_hubert import CustomHubert
-        from bark_hubert_quantizer.customtokenizer import CustomTokenizer
-
-        model = ('quantifier_V1_hubert_base_ls960_23.pth', 'tokenizer_large.pth') if large_quant_model else (
-            'quantifier_hubert_base_ls960_14.pth', 'tokenizer.pth')
-
-        print('Loading HuBERT...')
-        hubert_model = CustomHubert(
-            HuBERTManager.make_sure_hubert_installed(), device=device)
-        print('Loading Quantizer...')
-        quant_model = CustomTokenizer.load_from_checkpoint(
-            HuBERTManager.make_sure_tokenizer_installed(model=model[0], local_file=model[1]), device)
+        """Load the model into memory to make running multiple predictions efficient"""        
 
     def predict(
         self,
